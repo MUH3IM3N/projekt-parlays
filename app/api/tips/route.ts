9a60487c5b2f35fafb6a7e0001891bc695ba8556
MUH3IM3N <muheimen1@gmail.com>
2025-06-30 18:10:21 +0200

    fix

--- app/api/tips/route.ts
+++ app/api/tips/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { kv } from "@upstash/kv";
 
 export async function GET() {
+  // Hole alle Tipps aus KV
   const tips = (await kv.lrange("tips", 0, -1)) || [];
   return NextResponse.json(tips);
 }
-
